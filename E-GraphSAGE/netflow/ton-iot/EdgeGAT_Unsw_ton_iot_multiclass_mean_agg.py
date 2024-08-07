f2f3a00c89e1f8f39facc19ff17c71c788240e1d
zhaolong <superzhaolong@126.com>
2024-08-07 23:38:42 +0800

    添加EdgeGATKAN模型

--- E-GraphSAGE/netflow/ton-iot/EdgeGAT_Unsw_ton_iot_multiclass_mean_agg.py
+++ E-GraphSAGE/netflow/ton-iot/EdgeGAT_Unsw_ton_iot_multiclass_mean_agg.py
@@ -45,7 +45,7 @@ test_labels_file_path = 'multicalss_test_labels.npy'
 report_file_path = 'EdgeGAT_multicalss_classification_report.json'
 
 # 参数
-epochs = 3
+epochs = 1500
 best_model_file_path = 'EdgeGAT_multiclass_best_model.pth'
 
 # 尝试加载训练图和测试图，如果文件不存在则创建图并保存
